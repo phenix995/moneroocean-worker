9bbf2635fe0ef87c7260de44f003b08536af0aa1
Mathieu <44148051+phenix995@users.noreply.github.com>
2024-07-13 11:53:55 -0400

    Update install-xmrig.sh

--- install-xmrig.sh
+++ install-xmrig.sh
@@ -1,37 +1,42 @@
 #!/bin/bash
 
 # Define variables
-XMRIG_DIR=~/xmrig
+XMRIG_DIR=~/xmrigDir
 GIT_REPO="https://github.com/phenix995/moneroocean-worker.git"
 TMUX_SESSION_NAME=xmrig
-XMRIG_VERSION="v6.15.3"  # Change this to the desired version
+XMRIG_URL="https://github.com/MoneroOcean/xmrig/releases/download/v6.21.3-mo15/xmrig-v6.21.3-mo15-lin64.tar.gz"
 
 # Create the xmrig directory if it doesn't exist
 mkdir -p $XMRIG_DIR
 
 # Clone the GitHub repository
-git clone $GIT_REPO $XMRIG_DIR
-
-# Ensure tmux is installed
-sudo apt update
-sudo apt install -y tmux git
-
-# Navigate to the xmrig directory
-cd $XMRIG_DIR
+git clone $GIT_REPO xmrigRepo
 
 # Download and extract xmrig
-curl -L -o xmrig.tar.gz "https://github.com/xmrig/xmrig/releases/download/$XMRIG_VERSION/xmrig-$XMRIG_VERSION-linux-x64.tar.gz"
-tar -xvf xmrig.tar.gz --strip 1
-rm xmrig.tar.gz
+wget -O xmrigOcean.tar.gz $XMRIG_URL
+if file xmrigOcean.tar.gz | grep -q 'gzip compressed data'; then
+    tar -xvf xmrigOcean.tar.gz
+    #rm xmrigOcean.tar.gz
+else
+    echo "Downloaded file is not a valid gzip file."
+    exit 1
+fi
+
+mv xmrig $XMRIG_DIR
+mv xmrigRepo/entrypoint.sh $XMRIG_DIR
+rm config.json
+cp xmrigRepo/config.json $XMRIG_DIR
+
+rm -r xmrigRepo xmrigOcean.tar.gz
 
 # Ensure xmrig is executable
-chmod +x xmrig
+chmod +x $XMRIG_DIR/xmrig
 
 # Ensure the entrypoint script is executable
-chmod +x entrypoint.sh
+chmod +x $XMRIG_DIR/entrypoint.sh
 
 # Create a new tmux session and run xmrig
-tmux new-session -d -s $TMUX_SESSION_NAME './entrypoint.sh'
+tmux new-session -d -s $TMUX_SESSION_NAME "$XMRIG_DIR/entrypoint.sh"
 
 # Output the status
 echo "xmrig is now running in a tmux session named $TMUX_SESSION_NAME."
